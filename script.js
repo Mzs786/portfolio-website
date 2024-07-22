35353e829335257e9293d60d4a1309ddb21ef70d
Mzs786 <mzubairsaleem786@gmail.com>
2024-07-23 04:19:13 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const menuLinks = document.querySelector(".menu-links");
+
+  if (hamburgerIcon) {
+    hamburgerIcon.addEventListener("click", function () {
+      hamburgerIcon.classList.toggle("open");
+      menuLinks.classList.toggle("open");
+    });
+  }
+});
